61c787e849837c8796f73f04ea996663df928be9
rbeeler <ruthbeeler3@gmail.com>
2020-04-23 18:53:27 -0700

    changed some background colors to make it easier to see divs for now

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,10 +3,27 @@ import React from 'react';
 
 function About(){
   return(
+    
     <div className='About'>
-      <h1>Our Company</h1>
 
       <div className='Team-member'>
+        <h2> Team Member </h2>
+      </div>
+
+      <div className='Team-member'>
+        <h2> Team Member </h2>
+      </div>
+
+      <div className='Team-member'>
+        <h2> Team Member </h2>
+      </div>
+
+      <div className='Team-member'>
+        <h2> Team Member </h2>
+      </div>
+
+      <div className='Team-member'>
+        <h2> Team Member </h2>
       </div>
     </div>
 
