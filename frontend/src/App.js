7f1763ef99419ae0a2d65460da392d6182c0363d
rbeeler <ruthbeeler3@gmail.com>
2020-04-25 12:03:36 -0700

    created styles folder and separated .css files for organization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import './App.css';
 
-import Nav from './components/NavBar';
+import './styles/About.css';
+import './styles/Adplatforms.css';
+import './styles/App.css';
+import './styles/Contact.css';
+import './styles/Dashboard.css';
+import './styles/Login.css';
+import './styles/NavBar.css';
+import './styles/SignIn.css';
+
+
 import About from './components/About';
 import Adplatforms from './components/Adplatforms';
 import Contact from './components/Contact';
-import SignIn from './components/auth/SignIn';
 import Footer from './components/Footer';
+import Nav from './components/NavBar';
+import SignIn from './components/auth/SignIn';
+
+
 
 function App() {
   return (
